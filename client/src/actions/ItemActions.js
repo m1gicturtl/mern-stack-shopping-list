c8e9c077f13b2bd2a4d9a25619006e9022996c31
mpernichev <pernichevm@gmail.com>
2019-09-23 16:32:00 +0300

    finished implementing redux and connected to the backend

--- client/src/actions/ItemActions.js
+++ client/src/actions/ItemActions.js
@@ -1,7 +1,42 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+import axios from 'axios';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+export const getItems = () => dispach => {
+    dispach(setItemsLoading());
+    axios
+        .get('/api/items')
+        .then(res => 
+            dispach({
+                type: GET_ITEMS,
+                payload: res.data
+            })
+        )
 };
+
+export const addItem = (item) => dispach => {
+    axios
+        .post('/api/items', item)
+        .then(res => 
+            dispach({
+                type: ADD_ITEM,
+                payload: res.data
+            })
+        )
+};
+
+export const deleteItem = (id) => dispatch => {
+    axios
+        .delete(`/api/items/${id}`)
+        .then(res => 
+            dispatch({
+                type: DELETE_ITEM,
+                payload: id
+            })
+        )
+};
+
+export const setItemsLoading = () => {
+    return {
+        type: ITEMS_LOADING
+    }
+}
